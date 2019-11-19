dac244bb872b330097abcb1dd3e1ca35324162f7
JemyCheung <zhangwei@qiniu.com>
2019-11-19 11:39:41 +0800

    up_v7.5.0

---
+++ android-sdk/build/generated/source/buildConfig/debug/com/qiniu/android/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qiniu.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.qiniu.android";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 750;
+  public static final String VERSION_NAME = "7.5.0";
+}
